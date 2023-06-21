d2d7f36a33006c61bb791d6ea0189b4a2a7e1407
Stulta-Amiko <phh01010312@gmail.com>
2023-06-21 22:21:27 +0900

    add Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-import Users from './users/pages/Users';
-import NewPlace from './places/pages/NewPlace';
-import { Redirect, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import Users from './users/pages/Users'
+import NewPlace from './places/pages/NewPlace'
+import MainNavigation from './shared/components/Navigation/MainNavigation'
+import { Redirect, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path='/' exact>
-          <Users />
-        </Route>
-        <Route path='/places/new' exact>
-          <NewPlace />
-        </Route>
-        <Redirect to='/' />
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path='/' exact>
+            <Users />
+          </Route>
+          <Route path='/places/new' exact>
+            <NewPlace />
+          </Route>
+          <Redirect to='/' />
+        </Switch>
+      </main>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
